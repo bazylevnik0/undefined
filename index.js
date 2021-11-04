d4fd9588af94cbc842254fc6ca59b70eb25e6373
niko bazylev <42851684+bazylevnik0@users.noreply.github.com>
2021-11-04 13:07:22 +0300

    restrcturize code
    to more object oriented style
    add maps for code

--- index.js
+++ index.js
@@ -14,35 +14,8 @@ let context = canvas.getContext("webgl")
 const renderer = new THREE.WebGLRenderer( { canvas: canvas } );
       renderer.setSize( window.innerWidth/3, window.innerHeight/3 );
       
-//document.body.appendChild( canvas );
-
-const Menu         = {}
-const Select_car   = {}
-const Select_level = {}
-const Game         = {}
-
-//game
-const Global = {
-	speed : 1,
-	game_status : true
-}
-
-const Track = {
-	time : 0,
-	pos  : 0
-}
-      Track.count = function() {
-		let delta 
-		let r1 = Math.random()*2
-		let r2 = Math.floor(Math.random())*2
-		if (r2 == 0) {
-		      r2 = -1
-		}else r2 =  1
-		    delta = r1 * r2
-		return delta
-       }
-
-const Plane  = function(color,x,z) {
+//constructors
+const Plane  = function(color,x,z,w,h,v) {
 		this.geometry = new THREE.PlaneGeometry();
 		switch( color )  {
 			case "brown" : {
@@ -55,53 +28,127 @@ const Plane  = function(color,x,z) {
 				this.material = new THREE.MeshBasicMaterial( { color: 0x000000 } );
 			}break; 
 		}
-		this.mesh = new THREE.Mesh( this.geometry , this.material );
-		this.mesh.name = "ground_plane_"+(""+Math.random()).slice(2,)
-		this.mesh.rotation.x = (-90*Math.PI)/180 
-		this.mesh.position.x = x
-		this.mesh.position.z = z
-		if(color == "green") this.mesh.scale.set(1,1,)
-		else  this.mesh.scale.set(2,2,)
-		scene.add(this.mesh)
+		this.obj = new THREE.Mesh( this.geometry , this.material );
+		this.obj.name = "ground_plane_"+(""+Math.random()).slice(2,)
+		this.obj.position.x = x
+		this.obj.position.z = z
+		if(v)this.obj.rotation.x = (-90*Math.PI)/180 
+		if(w)this.obj.scale.x = w
+		if(h)this.obj.scale.y = h
+		if(color == "green") this.obj.scale.set(1,1,)
+		else  this.obj.scale.set(2,2,)
+		scene.add(this.obj)
 	       }
 
-var brown = new Plane("brown",0,6)
-var green = new Plane("green",0,6)
-var black = new Plane("black",0,6)
+
+//preload
+var brown = new Plane("brown",0,6,1,1,true)
+var green = new Plane("green",0,6,1,1,true)
+var black = new Plane("black",0,6,1,1,true)
 
 
-const Ground = {
-		data: [ [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [] ],
-		build: function() {
+//structure
+const Movie        = {}
+const Menu         = {}
+const Select_car   = {}
+const Select_level = {}
+const Game         = {}
+    /*Game
+	  global.
+		status.
+		car.
+	  local.
+		track.
+			count()
+		ground.
+			data[]
+			build()
+			move.
+				status
+				direction
+				run()
+				shift()
+				push()
+				convert_x()
+				conver_z()
+		sky
+		   obj
+		   items[]
+		   build()
+		out
+		   obj
+		   items[]
+		   build()
+    */
+
+      //g
+      Game.global = {
+		status : true
+      }
+		
+         Game.global.car = {
+		geometry : new THREE.BoxGeometry(),
+		material : new THREE.MeshBasicMaterial( { color: 0xffffff } )
+	 } 
+	    Game.global.car.build = function() {
+			Game.global.car.obj = new THREE.Mesh( Game.global.car.geometry , Game.global.car.material )	
+			Game.global.car.obj.name = "car"
+			Game.global.car.obj.position.z = 4
+			Game.global.car.obj.scale.set(1,1,2)
+			scene.add(Game.global.car.obj)
+	    }
+      //l
+      Game.local  = {
+		speed : 1
+      }
+         Game.local.track   = {
+		   time : 0,
+		   pos  : 0
+         }
+         Game.local.track.count = function() {
+		let delta 
+		let r1 = Math.random()*2
+		let r2 = Math.floor(Math.random())*2
+		if (r2 == 0) {
+		      r2 = -1
+		}else r2 =  1
+		    delta = r1 * r2
+		return delta
+         }
+	 //gr
+	 Game.local.ground = {
+         	data: [ [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [] ]
+	 }
+		Game.local.ground.build = function() {
 			for (let j = 0; j <= 18; j++){
 				for (let i = 0; i <= 18 ; i++){
 					if(i >= 8 && i <= 12 || j % 2 == 0) {
 						let temp = {
 							type: "red",
-							obj :  brown.mesh.clone()
+							obj :  brown.obj.clone()
 						}
-						Ground.data[j].push(temp)
-						temp.obj.position.x = Ground.convert_x(i)
-						temp.obj.position.z = Ground.convert_z(j)
+						Game.local.ground.data[j].push(temp)
+						temp.obj.position.x = Game.local.ground.move.convert_x(i)
+						temp.obj.position.z = Game.local.ground.move.convert_z(j)
 						scene.add(temp.obj)
 					} else {
 						let temp = {
 							type: "green",
-							obj :  green.mesh.clone()
+							obj :  green.obj.clone()
 						}
-						Ground.data[j].push(temp)
-						temp.obj.position.x = Ground.convert_x(i)
-						temp.obj.position.z = Ground.convert_z(j)
+						Game.local.ground.data[j].push(temp)
+						temp.obj.position.x = Game.local.ground.move.convert_x(i)
+						temp.obj.position.z = Game.local.ground.move.convert_z(j)
 						scene.add(temp.obj)
 					}
 				}
 			}
-		},
-		move: {
+		}
+		Game.local.ground.move = {
 			status : false,
 			direction : "straight",
 			run : function() {
-				Ground.move.status = true
+				Game.local.ground.move.status = true
 				for (let i = 1; i < 7; i ++) {
 					let time = Math.random()*5000 + 5000
 					let seed = Math.floor(Math.random()*2)
@@ -110,27 +157,27 @@ const Ground = {
 						case 0 : dir = "left" ; break;
 						case 1 : dir = "right"; break;
 					}
-					setTimeout( ()=>{Ground.move.direction = dir} , time * i  ) 
+					setTimeout( ()=>{Game.local.ground.move.direction = dir} , time * i  ) 
 				}
 				let timer = setInterval( function() {
-					if(Ground.move.status == false){
+					if(Game.local.ground.move.status == false){
 						clearInterval(timer)
 					} else {
-						Ground.data.forEach( line=>{
+						Game.local.ground.data.forEach( line=>{
 							line.forEach(  el=>{
-								el.obj.position.z += Global.speed;
+								el.obj.position.z += Game.local.speed;
 							})
 						})
-						if (Ground.data[0][0].obj.position.z > 5){
-							switch (Ground.move.direction) {
+						if (Game.local.ground.data[0][0].obj.position.z > 5){
+							switch (Game.local.ground.move.direction) {
 								case "straight" : {
 									if ( camera.rotation.y < 0) {
 										camera.rotation.y += 0.005
 									}else if(camera.rotation.y >= 0) {
 										camera.rotation.y -= 0.005
 									}
-									Ground.push("straight")
-									Ground.shift()
+									Game.local.ground.move.push("straight")
+									Game.local.ground.move.shift()
 								}break;	
 								case "left" : {
 									if ( camera.rotation.y < -0.25) {
@@ -138,8 +185,8 @@ const Ground = {
 									}else if(camera.rotation.y > -0.25) {
 										camera.rotation.y -= 0.025
 									}
-									Ground.push("left")
-									Ground.shift()
+									Game.local.ground.move.push("left")
+									Game.local.ground.move.shift()
 								}break;	
 								case "right" : {
 									if ( camera.rotation.y < 0.25) {
@@ -147,18 +194,17 @@ const Ground = {
 									}else if(camera.rotation.y > 0.25) {
 										camera.rotation.y -= 0.025
 									}
-									Ground.push("right")
-									Ground.shift()
+									Game.local.ground.move.push("right")
+									Game.local.ground.move.shift()
 								}break;
 							}
 						}
 					}
 				},100)
-			}
-		},
-		shift: function() {
+			},
+			shift: function() {
 			//for test:
-				let temp = Ground.data[0]  
+				let temp = Game.local.ground.data[0]  
 				temp.forEach(el=>{
 					scene.remove(el.obj)
 					el.obj = null
@@ -170,43 +216,43 @@ const Ground = {
 				for (let i = 0; i < temp.length; i++){
 					delete temp[i]
 				}
-				Ground.data[0] = null
-				Ground.data.shift()
+				Game.local.ground.data[0] = null
+				Game.local.ground.data.shift()
 			//
-		},
-		push:  function(direction) {
+			},
+			push:  function(direction) {
 			//for test:
-				Ground.data[18] = []
-					let delta = Track.count()
+				Game.local.ground.data[18] = []
+					let delta = Game.local.track.count()
 					let delta_time = Math.random()*30
-					Track.time += (delta_time*Math.PI)/180
-					Track.time = Track.time % 4
-					Track.pos = Math.sin(Track.time)*2 + delta
+					Game.local.track.time += (delta_time*Math.PI)/180
+					Game.local.track.time = Game.local.track.time % 4
+					Game.local.track.pos = Math.sin(Game.local.track.time)*2 + delta
 				if(direction == "left")  {
-					Track.pos < 5  ? Track.pos = 4  : false
-					setTimeout( () => {Ground.move.direction = "straight"} , 3000 )
+					Game.local.track.pos < 5  ? Game.local.track.pos = 4  : false
+					setTimeout( () => {Game.local.ground.move.direction = "straight"} , 3000 )
 			        }  
 				if(direction == "right")  {
-					Track.pos > -5  ? Track.pos = -4  : false
-					setTimeout( () => {Ground.move.direction = "straight"} , 3000 )
+					Game.local.track.pos > -5  ? Game.local.track.pos = -4  : false
+					setTimeout( () => {Game.local.ground.move.direction = "straight"} , 3000 )
 			        }  
 				let temp = []
 				for (let i = 0; i <= 18 ; i++){
-					if(i >= (Track.pos + 10)-3 && i <= (Track.pos + 10)+3 ) {
+					if(i >= (Game.local.track.pos + 10)-3 && i <= (Game.local.track.pos + 10)+3 ) {
 						temp[i] = {
 							type: "way",
-							obj :  brown.mesh.clone()
+							obj :  brown.obj.clone()
 							}
-						temp[i].obj.position.x = Ground.convert_x(i)
-						temp[i].obj.position.z = Ground.convert_z(18)
+						temp[i].obj.position.x = Game.local.ground.move.convert_x(i)
+						temp[i].obj.position.z = Game.local.ground.move.convert_z(18)
 						scene.add(temp[i].obj)
 					} else {
 						temp[i] = {
 							type: "green",
-							obj :  green.mesh.clone()
+							obj :  green.obj.clone()
 							}
-						temp[i].obj.position.x = Ground.convert_x(i)
-						temp[i].obj.position.z = Ground.convert_z(18)
+						temp[i].obj.position.x = Game.local.ground.move.convert_x(i)
+						temp[i].obj.position.z = Game.local.ground.move.convert_z(18)
 						scene.add(temp[i].obj)
 					}
 				}
@@ -216,19 +262,11 @@ const Ground = {
 					let seed_a_dir = Math.floor(Math.random()*2)
 					switch (seed_a_dir) {
 						case 0 : {					
-							temp[9].obj.material = black.mesh.material
-							temp[8].obj.material = black.mesh.material
-							temp[7].obj.material = black.mesh.material
-							temp[6].obj.material = black.mesh.material
-							temp[5].obj.material = black.mesh.material
+							temp[9].obj.material = temp[8].obj.material = temp[7].obj.material = temp[6].obj.material = temp[5].obj.material = black.obj.material
 							temp[9].type =	temp[8].type =	temp[7].type =	temp[6].type = temp[5].type = "black"
 						}break;
 						case 1 : {
-							temp[10].obj.material = black.mesh.material
-							temp[11].obj.material = black.mesh.material
-							temp[12].obj.material = black.mesh.material
-							temp[13].obj.material = black.mesh.material
-							temp[14].obj.material = black.mesh.material
+							temp[10].obj.material =	temp[11].obj.material = temp[12].obj.material = temp[13].obj.material = temp[14].obj.material = black.obj.material
 							temp[10].type = temp[11].type =	temp[12].type =	temp[13].type =	temp[14].type = "black"
 						}break;
 					} 
@@ -239,14 +277,14 @@ const Ground = {
 					switch (seed_a_dir) {
 						case 0 : {					
 							let seed_a_place = Math.floor(Math.random()*8)+4
-							temp[0+seed_a_place].obj.material = black.mesh.material
-							temp[1+seed_a_place].obj.material = black.mesh.material
+							temp[0+seed_a_place].obj.material = black.obj.material
+							temp[1+seed_a_place].obj.material = black.obj.material
 							temp[0+seed_a_place].type =	temp[1+seed_a_place].type = "black"
 						}break;
 						case 1 : {						
 							let seed_a_place = Math.floor(Math.random()*8)+4
-							temp[18-seed_a_place].obj.material = black.mesh.material
-							temp[17-seed_a_place].obj.material = black.mesh.material
+							temp[18-seed_a_place].obj.material = black.obj.material
+							temp[17-seed_a_place].obj.material = black.obj.material
 							temp[18-seed_a_place].type =	temp[17-seed_a_place].type = "black"
 						}break;
 					} 
@@ -257,113 +295,62 @@ const Ground = {
 					switch (seed_a_dir) {
 						case 0 : {					
 							let seed_a_place = Math.floor(Math.random()*8)+8
-							temp[0+seed_a_place].obj.material = black.mesh.material
+							temp[0+seed_a_place].obj.material = black.obj.material
 							temp[0+seed_a_place].type = "black"
 						}break;
 						case 1 : {						
 							let seed_a_place = Math.floor(Math.random()*8)+8
-							temp[18-seed_a_place].obj.material = black.mesh.material
+							temp[18-seed_a_place].obj.material = black.obj.material
 							temp[18-seed_a_place].type = "black"
 						}break;
 					} 
 				}
 			
-				Ground.data[18] = temp;
+				Game.local.ground.data[18] = temp;
 			//
-		},
-		convert_x: function(x) {
-			return x -10
-		},
-		convert_z: function(z) {
-			return (z -5)*(-1)
+			},
+			convert_x: function(x) {
+				return x -10
+			},
+			convert_z: function(z) {
+				return (z -5)*(-1)
+			}
+        }
+	//sk
+	Game.local.sky = {
+		geometry : new THREE.PlaneGeometry(),
+		material : new THREE.MeshBasicMaterial( { color: 0x0000ff } )
+	}
+		Game.local.sky.build = function() {
+			Game.local.sky.obj = new THREE.Mesh( Game.local.sky.geometry , Game.local.sky.material )	
+			Game.local.sky.obj.name = "sky"
+			Game.local.sky.obj.position.z = -16
+			Game.local.sky.obj.scale.set(150,150,)
+			scene.add(Game.local.sky.obj)
 		}
-}
-Ground.build()
-Ground.move.run()
-//for test:
-let Sky_geometry = new THREE.PlaneGeometry()
-let Sky_material = new THREE.MeshBasicMaterial( { color: 0x0000ff } )
-let Sky_mesh     = undefined
-    Sky_mesh = new THREE.Mesh( Sky_geometry , Sky_material )
-    Sky_mesh.name = "Sky"
-    Sky_mesh.position.z = -16
-    Sky_mesh.scale.set(150,150,)
-scene.add(Sky_mesh)	
-//
-
-
-//for test:
-let Out_geometry = new THREE.PlaneGeometry()
-let Out_material = new THREE.MeshBasicMaterial( { color: 0xffff00 } )
-let Out_mesh     = undefined
-    Out_mesh = new THREE.Mesh( Out_geometry , Out_material )
-    Out_mesh.name = "out"
-    Out_mesh.scale.set(150,150,)
-    Out_mesh.position.y = -0.1
-    Out_mesh.rotation.x = (-90*Math.PI)/180 
-scene.add(Out_mesh)	
-//
-
-//for test:
-let Car_geometry = new THREE.BoxGeometry()
-let Car_material = new THREE.MeshBasicMaterial( { color: 0xffffff } )
-let Car_mesh     = undefined
-    Car_mesh = new THREE.Mesh( Car_geometry , Car_material )
-    Car_mesh.name = "Car"
-    Car_mesh.position.z = 4
-    Car_mesh.scale.set(1,1,2)
-scene.add(Car_mesh)	
-
-document.addEventListener("keydown", function(event){
-	//console.log(event.code)
-	switch(event.code){
-		case "ArrowRight" : {
-			Car_mesh.rotation.y = (15*Math.PI)/180
-			Car_mesh.position.x += 0.25
-		}break;
-		case "ArrowLeft" : {
-			Car_mesh.rotation.y = (-15*Math.PI)/180
-			Car_mesh.position.x += -0.25
-		}break;
-		case "ArrowUp" : {
-			Global.speed += 0.025
-			Car_mesh.position.z -= 0.1
-			if(Car_mesh.position.z < 3.0) {
-				Car_mesh.position.z += 0.1
-				Global.speed -= 0.025
-			}
-		}break;		
-		case "ArrowDown" : {
-			Global.speed -= 0.025
-			Car_mesh.position.z += 0.1
-			if(Car_mesh.position.z > 4.0) {
-				Car_mesh.position.z -= 0.1
-				Global.speed += 0.025
-			}
-		}break;
+	//ou
+	Game.local.out = {
+		geometry : new THREE.PlaneGeometry(),
+		material : new THREE.MeshBasicMaterial( { color: 0xffff00 } )
 	}
-})
-document.addEventListener("keyup", function(event){
-	//console.log(event.code)
-	switch(event.code){
-		case "ArrowRight" : {
-			Car_mesh.rotation.y = 0
-		}break;
-		case "ArrowLeft" : {
-			Car_mesh.rotation.y = 0
-		}break;
-	}
-})
-let check = setInterval( function () {
-	if (Global.game_status == true) {
-		if(Car_mesh.position.x > 0) Car_mesh.position.x -= 0.005
-		if(Car_mesh.position.x < 0) Car_mesh.position.x += 0.005
-	}
-})
-//
+		Game.local.out.build = function() {
+			Game.local.out.obj = new THREE.Mesh( Game.local.out.geometry , Game.local.out.material )	
+			Game.local.out.obj.name = "out"
+			Game.local.out.obj.position.y = -0.1
+			Game.local.out.obj.scale.set(150,150,)
+    			Game.local.out.obj.rotation.x = (-90*Math.PI)/180 
+			scene.add(Game.local.out.obj)
+		}
 
 
 
+//load
+Game.local.sky.build()
+Game.local.out.build()
+Game.local.ground.build()
+Game.local.ground.move.run()
+Game.global.car.build()
+//show
 const animate = function () {
 
 	requestAnimationFrame( animate );
@@ -375,3 +362,56 @@ const animate = function () {
 };
 
 animate();
+
+//control
+
+document.addEventListener("keydown", function(event){
+	//console.log(event.code)
+	switch(event.code){
+		case "ArrowRight" : {
+			Game.global.car.obj.rotation.y = (15*Math.PI)/180
+			Game.global.car.obj.position.x += 0.25
+		}break;
+		case "ArrowLeft" : {
+			Game.global.car.obj.rotation.y = (-15*Math.PI)/180
+			Game.global.car.obj.position.x += -0.25
+		}break;
+		case "ArrowUp" : {
+			Game.local.speed += 0.025
+			Game.global.car.obj.position.z -= 0.1
+			if(Game.global.car.obj.position.z < 3.0) {
+				Game.global.car.obj.position.z += 0.1
+				Game.local.speed -= 0.025
+			}
+		}break;		
+		case "ArrowDown" : {
+			Game.local.speed -= 0.025
+			Game.global.car.obj.position.z += 0.1
+			if(Game.global.car.obj.position.z > 4.0) {
+				Game.global.car.obj.position.z -= 0.1
+				Game.local.speed += 0.025
+			}
+		}break;
+	}
+})
+document.addEventListener("keyup", function(event){
+	//console.log(event.code)
+	switch(event.code){
+		case "ArrowRight" : {
+			Game.global.car.obj.rotation.y = 0
+		}break;
+		case "ArrowLeft" : {
+			Game.global.car.obj.rotation.y = 0
+		}break;
+	}
+})
+let check = setInterval( function () {
+	if (Game.global.status == true) {
+		if(Game.global.car.obj.position.x > 0) Game.global.car.obj.position.x -= 0.005
+		if(Game.global.car.obj.position.x < 0) Game.global.car.obj.position.x += 0.005
+	}
+})
+//
+
+
+//system
