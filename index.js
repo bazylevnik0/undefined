249edb825f8cedc742cc09961352d17f8de4af20
bazylevnik0 <bazylevnik0@gmail.com>
2021-11-09 15:07:29 +0300

    add anim_a and anim_b

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 import * as THREE from "/build/three.module.js" 
 import { GLTFLoader } from '/build/GLTFLoader.js';
 const loader_car_red = new GLTFLoader();
+const loader_car_anim_a = new GLTFLoader();
+const loader_car_anim_b = new GLTFLoader();
+const loader_car_anim_c = new GLTFLoader();
+const loader_car_anim_s = new GLTFLoader();
 const clock = new THREE.Clock();
 		
 //constructors
@@ -209,6 +213,8 @@ const Game         = {}
 	requestAnimationFrame( Game.animate );
 	let mixerUpdateDelta = clock.getDelta();
 	Game.global.car.animation.mixer !== undefined ? Game.global.car.animation.mixer.update( mixerUpdateDelta ) : false
+	Game.global.car.animation.action.a_a.mixer !== undefined ? Game.global.car.animation.action.a_a.mixer.update( mixerUpdateDelta ) : false
+	Game.global.car.animation.action.a_b.mixer !== undefined ? Game.global.car.animation.action.a_b.mixer.update( mixerUpdateDelta ) : false
 	Game.renderer.render( Game.global.scene, Game.global.camera );
       };
 
@@ -242,7 +248,55 @@ const Game         = {}
 			arr : undefined,
 			mixer : undefined,
 			actions_loop : undefined,
-			
+			action : {
+				a_a : {
+					obj : undefined,
+					run : function() {
+						Game.global.car.animation.action.a_a.obj.position.x = Game.global.car.obj.position.x
+						Game.global.car.animation.action.a_a.obj.position.z = Game.global.car.obj.position.z
+						Game.global.car.animation.action.a_a.actions.forEach( el=> el.reset().play() )
+						Game.global.car.animation.actions_loop.forEach( el=>  el.stop())
+						let k = 100
+						let timer = setInterval( function() {	
+							if (k > 0) {
+								Game.global.car.obj.rotation.y -= k/50
+								k--
+							} else clearInterval(timer)
+						})
+						setTimeout( ()=> Game.global.car.animation.action.a_a.actions.forEach( el=> el.stop()) ,2500)
+					
+					}
+				},
+				a_b : {
+					obj : undefined,
+					run : function() {
+						console.log( Game.global.car.animation.action.a_b.actions )
+						Game.global.car.animation.action.a_b.obj.position.x = Game.global.car.obj.position.x
+						Game.global.car.animation.action.a_b.obj.position.z = Game.global.car.obj.position.z
+						Game.global.car.animation.action.a_b.actions.forEach( el=> el.play() )
+						setTimeout( ()=> Game.global.car.animation.action.a_b.actions.forEach( el=> el.stop()) ,500)
+					}
+				},
+				a_c : {
+					obj : undefined,
+					run : function() {
+
+					}
+				},
+				a_s : {
+					obj : undefined,
+					anim : function(log) {
+						switch (log) {
+							case true : {
+
+							}break;
+							case false : {
+
+							}break;
+						}
+					}
+				},
+			}
 		}
 	 } 
 	    Game.global.car.build = function() {
@@ -285,6 +339,58 @@ const Game         = {}
 			Game.global.car.animation.actions_loop.forEach( el=> el.reset().play() )
  			Game.global.car.loaded = true
 			}set()
+			//anims
+			let temp = ["a","b","c","s"]
+			for (let i = 0; i < 2; i++){
+				console.log(i)
+				let temp_loader
+				let temp_obj
+				let temp_length
+				switch(temp[i]){
+					case "a" : {
+						temp_loader = loader_car_anim_a;
+						temp_obj = Game.global.car.animation.action.a_a;
+						temp_length = 3;
+					}break;
+					case "b" : {
+						console.log("b")
+						temp_loader = loader_car_anim_b;
+						temp_obj = Game.global.car.animation.action.a_b;
+						temp_length = 2;
+					}break;
+				}
+				console.log(temp)
+				console.log(i)
+				console.log(temp[i])
+				temp_loader.load( '/src/anim_' + temp[i] +'.glb',  function ( gltf ) {
+					let model = gltf.scene;
+					model.scale.set(0.5, 0.4, 0.6)
+					model.traverse( function ( object ) {
+						if ( object.isMesh ) object.castShadow = true;
+					} );
+			
+					temp_obj.obj = model
+					let timer = setInterval( function() {
+						if( temp_obj.obj !== undefined ) {
+						clearInterval(timer)
+						temp_obj.arr = gltf.animations;
+						console.log(temp_obj.arr)
+						temp_obj.mixer = new THREE.AnimationMixer( model );
+						temp_obj.actions = []
+						for (let j = 0; j < temp_length; j++) {
+							temp_obj.actions[j] = temp_obj.mixer.clipAction( temp_obj.arr[ j ] );
+						}
+						temp_obj.actions.forEach( el=> {
+							//el.loop = THREE.LoopOnce
+							el.clampWhenFinished   = true
+							//el.reset().play()
+						})
+					
+						Game.global.scene.add( temp_obj.obj )
+						}
+					})
+				})
+			}
 	    }
       //l
       Game.local  = {
@@ -460,8 +566,8 @@ const Game         = {}
 						Game.global.scene.add(temp[i].obj)
 					}
 				}
-				//add black a
-				let seed_a = Math.floor(Math.random()*50)
+				//add black a				//!
+				let seed_a = Math.floor(Math.random()*10)
 				if(Game.local.ground.move.direction !== "straight") seed_a = 1
 				if  (seed_a == 0) {
 					let seed_a_dir = Math.floor(Math.random()*15)
@@ -636,8 +742,6 @@ const Game         = {}
 			Game.global.scene.add(Game.local.out.obj)
 		}
 
-
-
 //preload
 Game.preload = function () {
 	new Promise((resolve, reject) => {
@@ -712,33 +816,37 @@ Game.load = async function (){
 
 //control
 document.addEventListener("keydown", function(event){
-	switch(event.code){
-		case "ArrowRight" : {
-			Game.global.car.obj.rotation.y = (15*Math.PI)/180
-			Game.global.car.obj.position.x += 1.5 
-		}break;
-		case "ArrowLeft" : {
-			Game.global.car.obj.rotation.y = (-15*Math.PI)/180
-			Game.global.car.obj.position.x -= 1.5
-		}break;
-		case "ArrowUp" : {
-			Game.local.speed += 0.75
-			Game.global.car.obj.position.z -= 0.1
-			if(Game.global.car.obj.position.z < 3.0) {
-				Game.global.car.obj.position.z += 0.1
-				Game.local.speed -= 0.75
-			}
-		}break;		
+	if ( Game.local.ground.move.status == true) {
+		switch(event.code){
+			case "ArrowRight" : {
+				Game.global.car.obj.rotation.y = (15*Math.PI)/180
+				Game.global.car.obj.position.x += 1.5 
+			}break;
+			case "ArrowLeft" : {
+				Game.global.car.obj.rotation.y = (-15*Math.PI)/180
+				Game.global.car.obj.position.x -= 1.5
+			}break;
+			case "ArrowUp" : {
+				Game.local.speed += 0.75
+				Game.global.car.obj.position.z -= 0.1
+				if(Game.global.car.obj.position.z < 3.0) {
+					Game.global.car.obj.position.z += 0.1
+					Game.local.speed -= 0.75
+				}
+			}break;		
+		}
 	}
 })
 document.addEventListener("keyup", function(event){
-	switch(event.code){
-		case "ArrowRight" : {
-			Game.global.car.obj.rotation.y = 0
-		}break;
-		case "ArrowLeft" : {
-			Game.global.car.obj.rotation.y = 0
-		}break;
+	if ( Game.local.ground.move.status == true) {
+		switch(event.code){
+			case "ArrowRight" : {
+				Game.global.car.obj.rotation.y = 0
+			}break;
+			case "ArrowLeft" : {
+				Game.global.car.obj.rotation.y = 0
+			}break;
+		}
 	}
 })
 
@@ -752,18 +860,22 @@ let check = setInterval( function () {
 			if(el.type == "black_a"){
 				if( actual > el.obj.position.x - 0.25 &&  actual < el.obj.position.x +0.25) {
 					console.log("catch_a")
+					Game.global.car.animation.action.a_a.run()
 					Game.local.ground.move.status = false
+					clearInterval(check)
 				} 
 			}
 			if(el.type == "black_b"){
 				if( actual > el.obj.position.x - 1 &&  actual < el.obj.position.x +1) {
-					console.log("catch_b")
+					console.log("catch_b")	
+					Game.global.car.animation.action.a_b.run()
 					Game.local.speed /= 3
 				} 
 			}
 			if(el.type == "black_c"){
 				if( actual > el.obj.position.x - 1.25 &&  actual < el.obj.position.x +1.25) {
 					console.log("catch_c")
+					Game.global.car.animation.action.a_c.run()
 					Game.global.score -= 25
 				} 
 			}
